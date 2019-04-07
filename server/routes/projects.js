f66f91e3978ef81b83b8d8085a775062f3a9da0b
Kamal <kamalkaur936@gmail.com>
2019-04-07 18:14:18 -0400

    final commit

---
+++ server/routes/projects.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let router = express.Router();
+let jwt = require('jsonwebtoken');
+
+let passport = require('passport');
+
+let projectController = require('../controllers/projects');
+
+function requireAuth(req, res, next) {
+    // check if the user is logged in
+    if(!req.isAuthenticated()) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
+
+/* GET Contact List page - READ Operation */
+router.get('/',    projectController.displayProjectList);
+
+/* GET Route for the Add page 
+   this will display the Add page */
+router.get('/add',    projectController.displayAddPage);
+
+/* POST Route for processing the Add page */
+router.post('/add',    projectController.processAddPage);
+
+/* GET request - display the Edit page */
+router.get('/edit/:id',    projectController.displayEditPage);
+
+/* POST request - Update the database with data from the Edit Page */
+router.post('/edit/:id',    projectController.processEditPage);
+
+/* GET request to perform the delete action */
+router.get('/delete/:id',    projectController.performDelete);
+
+
+
+
+module.exports = router;
